793811a27b286a89b38f8c6bf5a522fe1453cf0c
dlievre <42@photeam.com>
2016-12-15 18:47:48 +0100

    save 15 dec

--- libft/ft_putbin.c
+++ libft/ft_putbin.c
@@ -6,34 +6,51 @@
 /*   By: dlievre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 18:46:15 by dlievre           #+#    #+#             */
-/*   Updated: 2016/12/14 19:01:29 by dlievre          ###   ########.fr       */
+/*   Updated: 2016/12/15 18:01:34 by dlievre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** affiche en binaire la variable passee, sous 4 ou 8  octeds
+** 4 = 0xFFFF ou 8 0xFFFFFFFF octeds
+** 4 = 16 bits 0b0000000000000000, 8 = 32bits
+** pour obtenir un mask de 32 bits on decale le mask a 1 de 32 fois a gauche
+*/
+
 #include "libft.h"
 
-int		ft_putbin(unsigned bin, int octed)
+int		ft_putbin(unsigned int bin, int nl)
 {
-	long mask;
+	int mask;
 	int n;
 
-	if (!bin || (octed != 4 && octed != 8))
-		return (1);
-	if (octed == 4)
+	mask = 0x8000;
+	n = 16;
+	while (n-- > 0)
 	{
-		mask = 0x8000;
-		n = 16;
+		ft_putchar((mask & bin) == 0x8000 ? '1' : '0');
+		bin = bin << 1;
 	}
-	if (octed == 8)
-	{
-		mask = 0x80000000;
-		n = 32;
-	}
+	if (nl == 1)
+		ft_putchar('\n');
+	return (0);
+}
 
+int		ft_putbin32(unsigned long bin, int nl)
+{
+	unsigned long	mask;
+	int				n;
+
+	mask = 1;
+	n = 0;
+	while (n++ < 32)
+		mask = mask << 1;
 	while (n-- > 0)
 	{
 		ft_putchar((mask & bin) == mask ? '1' : '0');
 		bin = bin << 1;
 	}
+	if (nl == 1)
+		ft_putchar('\n');
 	return (0);
 }
