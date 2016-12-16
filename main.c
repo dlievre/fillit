7c1be01ad3a4acaf4fe5514dab8f95cf13058c40
dlievre <42@photeam.com>
2016-12-16 18:18:26 +0100

    nouvelle version

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dlievre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/12 15:05:05 by dlievre           #+#    #+#             */
-/*   Updated: 2016/12/14 18:41:30 by dlievre          ###   ########.fr       */
+/*   Updated: 2016/12/16 18:15:46 by dlievre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
 
 int		*read_elem(char *file,int *tbltetrim)
 {
-	int			fd;
-	char		*buf;
-	int			notetrim;
-	int			bin;
+	int		fd;
+	char	*buf;
+	int		notetrim;
+	int		bin;
 
 
 	buf = malloc(26 * (16 + 4 + 1) * sizeof(char));
@@ -31,8 +31,8 @@ int		*read_elem(char *file,int *tbltetrim)
 	notetrim = 0;
 	while (read(fd, buf, (16 + 4 + 1)) > 0)
 	{
-	printf("\nRead element : %d\n", notetrim);
-	bin = 0b0000000000000000;
+		printf("\nRead Element : %d ", notetrim); ft_putchar('A' + notetrim);ft_putchar('\n');
+		bin = 0b0000000000000000;
 		while (*buf)
 		{
 			ft_putchar(*buf);
@@ -43,7 +43,7 @@ int		*read_elem(char *file,int *tbltetrim)
 			buf++;
 		}
 		tbltetrim[notetrim] = bin;
-		ft_putnbr(notetrim); ft_putchar(' ');ft_putbin(bin, 4);
+		ft_putnbr(notetrim); ft_putchar(' ');ft_putbin(bin, 16, '\n');
 
 		notetrim++;
 	}
@@ -51,40 +51,66 @@ int		*read_elem(char *file,int *tbltetrim)
 	return (tbltetrim);
 }
 
-void		ft_tblinit(int	*tbl, size_t len)
-{
-	while (len-- > 0)
-	*tbl++ = '\0';
-}
-
 int		main(int argc, char **argv )
 {
 	int			tbltetrim[26];
 	int			n;
-	long		tetris;
-	long		result;
+	int		tetris;
+	int		result;
+	int 	mask;
+	int		i;
+	char	*ok;
+
 
 	ft_tblinit(tbltetrim, 26);
 	if (!(read_elem(argv[1], tbltetrim)))
 		ft_putstr("\nusage: fillit source_file\n\n");
 	else
-	ft_putstr("\ntbl tetriminos\n");
+		ft_putstr("\ntbl tetriminos\n");
 	n = 0;
 	while (tbltetrim[n] != '\0')
 	{
-		ft_putbin(tbltetrim[n], 4);
-		ft_putchar('\n');
+		ft_putbin(tbltetrim[n], 16, '\n');
 		n++;
 	}
-
-	while (n > 0)
+	ft_putstr("\ncalage a droite des tetriminos\n");
+	n = 0;
+	mask = 0xFFFFFFF0;
+	while (tbltetrim[n] != '\0')
 	{
-	ft_putbin(result, 8);
-	if ((tetris | tbltetrim[n]) >= (tetris + tbltetrim[n]))
-		result = tetris | tbltetrim[n];
-	ft_putbin(result, 8);
-	n++;
+		while ((tbltetrim[n] | mask) == mask)
+			tbltetrim[n] = tbltetrim[n] >> 4;
+		ft_putbin(tbltetrim[n],8, '\n');
+		n++;
 	}
+	ok = malloc(sizeof(char) * n);
+
+	ft_putstr("\ntest tetris\n");
+	n = 0;
+	result = 0;
+	while (tbltetrim[n] != '\0')
+	{
+		ft_putbin(result, 32, 'r');ft_putbin(tbltetrim[n],8 ,'t');
+		ft_putbin((result ^ tbltetrim[n]), 32, '^');
+		for (i = 8 ; i > 0 ; i--)
+		{
+			if ((result ^ tbltetrim[n]) >= (result | tbltetrim[n]))
+			{
+				result = result ^ tbltetrim[n];
+				ok[n] = 'A' + n;
+				i = 0;
+			}
+			else
+			{
+			//	ft_putchar('.');
+				tbltetrim[n] = tbltetrim[n] << 4;
+			}
+		}
+		ft_putbin(result, 32, '\n');
+		n++;
+
+	}
+ft_putstr("ordre des tetriminos integre : ");ft_putstr(ok);
 	return (0);
 }
 
